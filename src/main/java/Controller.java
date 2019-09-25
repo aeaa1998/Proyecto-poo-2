881ba00e7821db3b5b2f9939b9ad4c0be89cd0da
Martín España <martineduardoer00@gmail.com>
2019-09-25 09:24:09 -0600

    Empece el metodo menu

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -13,6 +13,33 @@ public class Controller {
     public Controller() {
 
         myConnection = new Connection();
+		view = new View();
 
     }
+	
+	public Menu(){
+		boolean active = true;
+		while(active){
+			switch(view.input("Ingrese la opción que desee: \nSolicitar jornada \nContacto \nEmergencia \nVer jornadas por voluntario \nSalir")){
+				case "Solicitar jornada":
+					//Code
+				break;
+				case "Contacto":
+					//Code 
+				break;
+				case "Emergencia":
+					//Code 
+				break;
+				case "Ver jornadas por voluntario":
+					//Code 
+				break;
+				case "Salir":
+					view.print("Gracias por utilizar este prototipo.");
+					active = false;
+				break;
+				default: 
+					view.print("Ingrese una opcion valida.");
+			}
+		}
+	}
 }
